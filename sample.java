bab5df01248281c7c8df42c3166b90220ec3b857
Iamneo <techadmin@iamneo.ai>
2023-08-30 11:04:44 +0000

    Backup Commit

--- sample.java
+++ sample.java
@@ -1,6 +1,7 @@
 class sample {
     public static void main(String args[])
     {
-        
+        System.out.println("hello world");
+        System.out.println("hellow ordl");
     }
 }
---
+++ test2.java
@@ -0,0 +1,5 @@
+public class test2 {
+    public static void public static void main(String[] args) {
+        
+    }
+}
